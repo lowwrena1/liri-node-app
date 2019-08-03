3e6f9689d52cfdfab83e6fad83f1ff7bd3676d28
Lorena Ontiveros <lorena0328o@gmail.com>
2019-08-02 20:15:07 -0500

    not fully sure but it works!

--- LiriApp.js
+++ LiriApp.js
@@ -2,15 +2,36 @@ require("dotenv").config();
 //allows reading for the keys file
 var keys = require("./keys.js");
 //access to spotify
-var spotify = require('node-spotify-api');
-var spotify = new spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//call function
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response);
+  })
 // bands in town request
-var moment = require('moment');
+//var moment = require('moment');
 //axio stuff
-var axios = require('axios');
+//var axios = require('axios');
 //chalk intergration
-var chalk = require ('chalk');
-//concert-this command
+//var chalk = require ('chalk');
+//- concert-this command
+ /*
+function bandsInTown () {
+    axios.get(querlyURL)
+    .then(function(Res){
+        console.log() //
+        console.log() //name of venue
+        console.log()// venue location
+        console.log() //date of event (mm/dd/yyyy)
+    })
+} 
+*/
 //- spotify-this-song command
+
+
 //- movie-this command
+
+
 //- do-what-it-says command
